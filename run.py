630237ed8aaade51b04d7f5dd8b4d2b61f47c58d
Your Name <you@example.com>
2024-05-29 23:44:12 +0200

    attempt fix

--- run.py
+++ run.py
@@ -10,161 +10,173 @@ import shutil
 ZOOM = 0.625
 
 
-
-
-
 with sync_playwright() as playwright:
-	browser = playwright.chromium.launch(headless=False)
-	context = browser.new_context(storage_state="session.json" if 'session.json' in os.listdir('.') else None)
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(
+        storage_state="session.json" if "session.json" in os.listdir(".") else None
+    )
 
-	page = context.new_page()
-	page.goto('https://www.everand.com/login', wait_until='domcontentloaded')
-	
-	page.locator("div.user_row").wait_for(state='attached', timeout=0)
+    page = context.new_page()
+    page.goto("https://www.everand.com/login", wait_until="domcontentloaded")
 
-	print('Logged in successfully.')
+    page.locator("div.user_row").wait_for(state="attached", timeout=0)
 
-	storage = context.storage_state(path="session.json")
-	context.close()
-	browser.close()
+    print("Logged in successfully.")
 
-	# Get the total number of command-line arguments
-	total_args = len(sys.argv)
-	# Initialize an empty list to store the cache directories
-	cache_dirs = []
-	book_filenames = []
+    storage = context.storage_state(path="session.json")
+    context.close()
+    browser.close()
 
+    # Get the total number of command-line arguments
+    total_args = len(sys.argv)
+    # Initialize an empty list to store the cache directories
+    cache_dirs = []
+    book_filenames = []
 
+    # Iterate over the arguments
+    for i in range(1, total_args):
+        book_url = sys.argv[i]
 
-	# Iterate over the arguments
-	for i in range(1, total_args):
-		book_url = sys.argv[i]
+        # create cache dir
+        book_filename = book_url.split("/")[5]
+        book_filenames.append(book_filename)
+        current_cache_directory = f"{os.getcwd()}/{book_filename}"
+        cache_dirs.append(current_cache_directory)
+        try:
+            os.mkdir(current_cache_directory)
+        except FileExistsError:
+            pass
+        print(f"Starting process of downloading book {i}: {sys.argv[i]}")
 
-		# create cache dir
-		book_filename = book_url.split('/')[5]
-		book_filenames.append(book_filename)
-		current_cache_directory = f'{os.getcwd()}/{book_filename}'
-		cache_dirs.append(current_cache_directory)
-		try:
-			os.mkdir(current_cache_directory)
-		except FileExistsError:
-			pass
-		print(f"Starting process of downloading book {i}: {sys.argv[i]}")
+        browser = playwright.chromium.launch(headless=True)
 
-	
+        print("Loading viewer...")
 
-		browser = playwright.chromium.launch(headless=True)
+        context = browser.new_context(
+            storage_state="session.json",
+            viewport={"width": 1200, "height": 1600},
+            ignore_https_errors=True,
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36",
+        )
+        context.set_extra_http_headers({"Accept-Language": "en-US,en;q=0.9"})
 
-		print('Loading viewer...')
+        page = context.new_page()
+        page.goto(book_url.replace("book", "read"))
 
-		context = browser.new_context(
-			storage_state=  "session.json",
-			viewport={'width': 1200, 'height': 1600},
-			ignore_https_errors = True,
-			user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
-		)
-		context.set_extra_http_headers({'Accept-Language': 'en-US,en;q=0.9'})
+        if "Browser limit exceeded" in page.content():
+            context.close()
+            browser.close()
+            sys.exit(
+                "You have tried to read this from too many computers or web browsers recently, and will need to wait up to 24 hours before returning to this book."
+            )
 
-		page = context.new_page()
-		page.goto(book_url.replace('book', 'read'))
+        # retrieve fonts
+        font_style = page.locator("#fontfaces").inner_html()
 
-		if 'Browser limit exceeded' in page.content():
-			context.close()
-			browser.close()
-			sys.exit('You have tried to read this from too many computers or web browsers recently, and will need to wait up to 24 hours before returning to this book.')
+        # open display menu
+        page.locator(".icon-ic_displaysettings").wait_for(state="visible")
+        page.evaluate(
+            "() => document.querySelector('.icon-ic_displaysettings').click()"
+        )
 
-		# retrieve fonts
-		font_style = page.locator('#fontfaces').inner_html()
+        # change to vertical mode
+        page.locator(".vertical_mode_btn").wait_for(state="visible")
+        page.evaluate("() => document.querySelector('.vertical_mode_btn').click()")
 
-		# open display menu
-		page.locator('.icon-ic_displaysettings').wait_for(state='visible')
-		page.evaluate("() => document.querySelector('.icon-ic_displaysettings').click()")
+        # open toc menu
+        page.locator('div.vertical_page[data-page="0"]').wait_for(state="visible")
+        page.evaluate("() => document.querySelector('.icon-ic_toc_list').click()")
+        chapter_selector = page.locator('li.text_btn[role="none"]')
+        chapter_selector.nth(0).wait_for(state="visible")
 
-		# change to vertical mode
-		page.locator('.vertical_mode_btn').wait_for(state='visible')
-		page.evaluate("() => document.querySelector('.vertical_mode_btn').click()")
+        # retrieve the number of chapters
+        num_of_chapters = chapter_selector.count()
 
-		# open toc menu
-		page.locator('div.vertical_page[data-page="0"]').wait_for(state='visible')
-		page.evaluate("() => document.querySelector('.icon-ic_toc_list').click()")
-		chapter_selector = page.locator('li.text_btn[role="none"]')
-		chapter_selector.nth(0).wait_for(state='visible')
+        # load the first chapter
+        page.evaluate(
+            "() => document.querySelector('li.text_btn[data-idx=\"0\"]').click()"
+        )
+        chapter_no = 1
 
-		# retrieve the number of chapters
-		num_of_chapters = chapter_selector.count()
+        # to render the chapter pages and save them as pdf
+        render_page = context.new_page()
+        render_page.set_viewport_size({"width": 1200, "height": 1600})
 
-		# load the first chapter
-		page.evaluate("() => document.querySelector('li.text_btn[data-idx=\"0\"]').click()")
-		chapter_no = 1
+        while True:
 
-		# to render the chapter pages and save them as pdf
-		render_page = context.new_page()
-		render_page.set_viewport_size({"width": 1200, "height": 1600})
+            page.locator('div.vertical_page[data-page="0"]').wait_for()
 
-		while True:
+            chapter_pages = page.locator("div.vertical_page")
+            number_of_chapter_pages = chapter_pages.count()
 
-			page.locator('div.vertical_page[data-page="0"]').wait_for()
+            print(
+                f"Downloading chapter {chapter_no}/{num_of_chapters} ({number_of_chapter_pages} pages)"
+            )
 
-			chapter_pages = page.locator('div.vertical_page')
-			number_of_chapter_pages = chapter_pages.count()
+            merger = PdfMerger()
 
-			print(f'Downloading chapter {chapter_no}/{num_of_chapters} ({number_of_chapter_pages} pages)')
+            page_no = 1
 
-			merger = PdfMerger()
+            while True:
 
-			page_no = 1
+                page_elem = chapter_pages.nth(page_no - 1)
+                html = page_elem.inner_html()
 
-			while True:
+                # replace img urls
+                html = html.replace('src="/', 'src="https://www.scribd.com/')
 
-				page_elem = chapter_pages.nth(page_no-1)
-				html = page_elem.inner_html()
+                # set page size
+                match = re.findall("width: ([0-9.]+)px; height: ([0-9.]+)px;", html)[0]
+                width, height = float(match[0]), float(match[1])
+                style = f"@page {{ size: {width*ZOOM}px {height*ZOOM}px; margin: 0; }} @media print {{ html, body {{ height: {height*ZOOM}px; width: {width*ZOOM}px;}}}}"
+                html = re.sub(
+                    'data-colindex="0" style="', 'data-colindex="0" x="', html
+                )
+                html = re.sub(
+                    'position: absolute.*?"',
+                    f'overflow: hidden; height: {height}px; width: {width}px; white-space: nowrap; zoom: {ZOOM};"',
+                    html,
+                )
 
-				# replace img urls
-				html = html.replace('src="/', 'src="https://www.scribd.com/')
+                # render page
+                content = f"<style>{style}{font_style}</style>{html}"
+                render_page.set_content(content)
 
-				# set page size
-				match = re.findall('width: ([0-9.]+)px; height: ([0-9.]+)px;', html)[0]
-				width, height = float(match[0]), float(match[1])
-				style = f'@page {{ size: {width*ZOOM}px {height*ZOOM}px; margin: 0; }} @media print {{ html, body {{ height: {height*ZOOM}px; width: {width*ZOOM}px;}}}}'
-				html = re.sub('data-colindex="0" style="', 'data-colindex="0" x="', html)
-				html = re.sub('position: absolute.*?"', f'overflow: hidden; height: {height}px; width: {width}px; white-space: nowrap; zoom: {ZOOM};"', html)
+                # print pdf
+                pdf_file = f"{current_cache_directory}/{chapter_no}_{page_no}.pdf"
+                render_page.pdf(path=pdf_file, prefer_css_page_size=True)
+                merger.append(pdf_file)
 
-				# render page
-				content = f'<style>{style}{font_style}</style>{html}'
-				render_page.set_content(content)
+                if page_no == number_of_chapter_pages:
+                    merger.write(f"{current_cache_directory}/{chapter_no}.pdf")
+                    merger.close()
+                    break
 
-				# print pdf
-				pdf_file = f'{current_cache_directory}/{chapter_no}_{page_no}.pdf'
-				render_page.pdf(path=pdf_file, prefer_css_page_size = True)
-				merger.append(pdf_file)
+                page_no += 1
 
-				if page_no == number_of_chapter_pages:
-					merger.write(f"{current_cache_directory}/{chapter_no}.pdf")
-					merger.close()
-					break
+            if chapter_no == num_of_chapters:
+                break
 
-				page_no += 1
+            page.evaluate(
+                "() => document.querySelectorAll('button.load_next_btn')[0].click()"
+            )
 
-			if chapter_no == num_of_chapters:
-				break
+            time.sleep(1)
+            chapter_no += 1
 
-			page.evaluate("() => document.querySelectorAll('button.load_next_btn')[0].click()")
-
-			time.sleep(1)
-			chapter_no += 1
-
-print('Merging PDF pages...')
+print("Merging PDF pages...")
 merger = PdfMerger()
-#TODO need to iterate over book_filenames and also add iteration over an array of chapter nos saved from the process above into an array
-book_filenames
-for cache_dir in cache_dirs:
-	for chapter_no in range(1, num_of_chapters+1):
-		merger.append(f"{cache_dir}/{chapter_no}.pdf")
+# TODO need to iterate over book_filenames and also add iteration over an array of chapter nos saved from the process above into an array
 
-	merger.write(f"{book_filename}.pdf")
-	merger.close()
+for i in range(1, total_args):
+    print('attempting merge of book number: ' + i)
+    for chapter_no in range(1, num_of_chapters + 1):
+        merger.append(f"{cache_dirs[i]}/{chapter_no}.pdf")
 
-	# delete cache dir
-	shutil.rmtree(cache_dir)
+    merger.write(f"{book_filenames[i]}.pdf")
+    merger.close()
 
-print('Download completed, enjoy your book!')
+    # delete cache dir
+    shutil.rmtree(cache_dirs[i])
+
+print("Download completed, enjoy your book!")
